7c6d6e862cd4507b96dd5b18392626d1a233640a
Pavan Kumar Penjandra <153838724+pavanpenjandrascientist@users.noreply.github.com>
2023-12-16 19:36:26 +0530

    requirements updated

--- template.py
+++ template.py
@@ -2,12 +2,14 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format="[%(asctime)]:%(message)s%")
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s:")
+
 
 project_name = "cnnClassifier"
 
 list_of_files = [
-    ".github/workflows/.gitkeep" f"src/{project_name}/__init__.py",
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/config/__init__.py",
@@ -20,8 +22,10 @@ list_of_files = [
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trails.ipynb",
+    "research/trials.ipynb",
+    "templates/index.html",
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
@@ -29,12 +33,12 @@ for filepath in list_of_files:
 
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory;{filedir}for the file name{filename}")
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) != 0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
-            logging.info(f"Creating empty file:{filepath}")
+            logging.info(f"Creating empty file: {filepath}")
 
     else:
         logging.info(f"{filename} is already exists")
